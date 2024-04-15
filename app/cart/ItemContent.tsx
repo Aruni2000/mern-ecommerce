e26933054b72195566422650cfb6b2c3fb1c2770
Nethmini799 <117433954+DreamyD20@users.noreply.github.com>
2024-04-15 21:59:42 +0530

    Create a ItemContent

---
+++ app/cart/ItemContent.tsx
@@ -0,0 +1,73 @@
+"use client"
+import { useState } from 'react';
+import Link from "next/link";
+import { CartProductType } from "../product/ProductDetails";
+import { formatPrice } from "@/utils/formatPrice";
+import { truncateText } from "@/utils/truncateText";
+import Image from "next/image";
+import SetQuanity from "../components/products/SetQuanity";
+
+interface ItemContentProps{
+    item: CartProductType;
+}
+
+const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
+    const [quantity, setQuantity] = useState(item.quantity);
+    const [total, setTotal] = useState(item.price * item.quantity);
+
+    const handleQtyIncrease = () => {
+        const newQuantity = quantity + 1;
+        setQuantity(newQuantity);
+        setTotal(item.price * newQuantity);
+    };
+
+    const handleQtyDecrease = () => {
+        if (quantity > 1) {
+            const newQuantity = quantity - 1;
+            setQuantity(newQuantity);
+            setTotal(item.price * newQuantity);
+        }
+    };
+
+    return (
+        <div className="grid grid-cols-4 text-xs md:text-sm gap-4 border-t-[1.5px] border-slate-200 py-4 items-center">
+            <div className="col-span-2 justify-self-start flex gap-5 md:gap-9">
+                <Link href={`/product/${item.id}`}>
+                    <div className="relative w-[70px] aspect-square">
+                        <Image src={item.selectedImg.image}
+                        alt={item.name}
+                        fill
+                        className="object-contain"/>
+                    </div>
+                </Link>
+                
+                <div className="flex flex-col justify-between">
+                    <Link href={`/product/${item.id}`}>
+                        {truncateText(item.name)} </Link>
+                    <div>{item.selectedImg.color}</div>
+                    <div className="w-[70px]">
+                        <button className="text-slate-500
+                        underline" onClick={() => {}}>Remove
+                        </button>
+                    </div>
+                </div>
+                
+            <div className="justify-self-center">
+                {formatPrice(item.price)}</div>
+            <div className="justify-self-center">
+                <SetQuanity
+                   cartCounter={true}
+                   cartProduct={item}
+                   handleQtyIncrease={handleQtyIncrease}
+                   handleQtyDecrease={handleQtyDecrease}
+                   />
+            </div>
+            <div className="justify-self-end font-semibold">
+                {formatPrice(total)}
+            </div>
+            </div>
+        </div >
+    );
+};
+ 
+export default ItemContent;
