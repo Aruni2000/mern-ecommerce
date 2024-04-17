9f498b1d6cca98402bb5c9d0c344d64c25ca5ec5
Nethmini799 <117433954+DreamyD20@users.noreply.github.com>
2024-04-18 01:26:36 +0530

    Create a UserMenu

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Container  from "../Container";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 
 const NavBar = () => {
@@ -33,7 +34,7 @@ const NavBar = () => {
                 <div className="hidden md:block">Search</div>
                 <div className="flex items-center gap-8 md:gap-12">
                     <CartCount/>
-                    <div>UserMenu</div>
+                    <UserMenu/>
                 </div>
               </div>
             </Container>
